3453a32133ac3fa81fb8db66b440fd3a96d46f75
RafxDev <jrafaelmorantess@gmail.com>
2026-06-16 15:18:30 -0500

    enseñando datos number y string

---
+++ 3. Datos Number y String/script.js
@@ -0,0 +1,15 @@
+//  -------------- Numeros ---------------
+
+let variableNumero = 10 //numero entero
+let variableDecimal = 3.14 //numero flotante
+
+
+// -------------- Strings (Texto) ---------------
+let variableTexto = 'Texto' //Valor String (cadena de caracteres con comillas simples)
+let variableTexto1 = "Texto" //Valor String (cadena de caracteres con comillas dobles)
+
+
+// -------------- String Texto Concatenado ---------------
+//Valor String (cadena de caracteres con comillas invertidas o backticks) permite concatenar variables dentro 
+// del texto usando ${} y saltos de linea
+let variableTexto2 = `Esta es una variable distinta ${variableTexto1}` 
