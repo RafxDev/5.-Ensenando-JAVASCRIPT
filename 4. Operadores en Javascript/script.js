baafd976a4a90ccdb84085b8afc2ae126c904ec1
RafxDev <jrafaelmorantess@gmail.com>
2026-06-16 15:38:00 -0500

    enseñando operadores

---
+++ 4. Operadores en Javascript/script.js
@@ -0,0 +1,46 @@
+// ----------- Operadores en JavaScript -------------
+
+
+//  ------  Operador Asignación (=)  ------
+let a = 5;
+let b = 10;
+
+
+//  ------  Operadores Aritméticos  ------
+
+//Suma (+)
+let suma = a + b;
+
+//Resta (-)
+let resta = a - b;
+
+//Multiplicación (*)
+let multiplicacion = a * b;
+
+//División (/)
+let division = a / b;
+
+//Resto (%)
+let resto = a % b;
+
+//Exponenciación (**)
+let exponente = a ** 2;
+
+
+//  ------  Incremento y Decremento  ------
+
+//Incremento (++)
+a++; // a ahora es 6
+
+//Decremento (--)
+b--; // b ahora es 9
+
+
+
+// ------ Console.log para mostrar resultados ------
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
+console.log("Resto:", resto);
+console.log("Exponenciación:", exponente);
