9bba37319e4f91a144d2e244ed74b843bd8157c4
RafxDev <jrafaelmorantess@gmail.com>
2026-06-17 11:59:19 -0500

    enseñando sintaxis de una variable

---
+++ 5. Sintaxis de las Variables/script.js
@@ -0,0 +1,24 @@
+//Maneras Nombrar Variables
+
+//1. Camel Case
+const nombreCompleto = "Juan Perez";
+
+//2. Snake Case
+const nombre_completo = "Juan Perez";
+
+//3. Pascal Case
+const NombreCompleto = "Juan Perez";
+
+//4. Kebab Case
+const $NombreCompleto = "Juan Perez";
+
+//5. Upper Case
+const _NombreCompleto = "Juan Perez";
+
+
+//Reglas para nombrar variables
+//1. No pueden comenzar con un número
+//2. No pueden contener espacios
+//3. No pueden contener caracteres especiales (excepto $ y _)
+//4. No pueden ser palabras reservadas (como var, let, const, etc.)
+//5. Deben ser descriptivas y claras para entender su propósito
